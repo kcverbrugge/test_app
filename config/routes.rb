395924897be2d0ccac37e980c02db2e11ae585a8
kcverbrugge <kcverbrugge@gmail.com>
2025-11-05 20:31:51 -0700

    I did something

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,5 +14,5 @@ Rails.application.routes.draw do
   resources :pages
 
   root "pages#home"
-  get "about",to: 'pages#about' # get request to the about page
+  get "about", to: 'pages#about' # get request to the about page
 end
